ac95dbef4aa48a438d0dcecaf9fec84200bc36e6
Marlon Rodriguez Rivera <110213632+marlon7piri@users.noreply.github.com>
2023-11-05 17:27:01 -0500

    add the scripts start to json

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import { MONGO_URL } from "./config.js";
 const ConnectDB = () => {
 
 
-  /* const url = "mongodb+srv://marlon7piri:marlonrodri7@mycluster.zm2z014.mongodb.net/alalmadb?retryWrites=true&w=majority" */
+ 
  try {
   mongoose.connect(MONGO_URL).then(() => {
     console.log("connecting a la base de datos");
