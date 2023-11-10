e37c8f1ace0ec0dbab1adeabc9ab52c5a31e5566
Marlon Rodriguez Rivera <110213632+marlon7piri@users.noreply.github.com>
2023-11-10 08:45:59 -0500

    changed the db file

--- db.js
+++ db.js
@@ -2,16 +2,15 @@ import mongoose from "mongoose";
 import { MONGO_URL } from "./config.js";
 
 const ConnectDB = () => {
-
-
- 
- try {
-  mongoose.connect(MONGO_URL).then(() => {
-    console.log("connecting a la base de datos");
-  });
- } catch (error) {
-  console.log(error)
- }
+  try {
+    mongoose.connect(MONGO_URL).then(() => {
+      console.log("connecting a la base de datos");
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    mongoose.connection.close();
+  }
 };
 
 export default ConnectDB;
